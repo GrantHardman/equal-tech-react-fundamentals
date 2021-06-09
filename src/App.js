9bccc9cb365369c2aa67c9358a04a26969328cff
Grant Hardman <ghardman@and.digital>
2021-06-09 19:58:59 +0100

    Add register page and connect routing for all pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Products from './Pages/Products/Products';
+import Login from './Pages/Login/Login';
+import Cart from './Pages/Cart/Cart';
+import Checkout from './Pages/Checkout/Checkout';
+import Register from './Pages/Register/Register';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/products">
             <Products />
+          </Route>
+          <Route path="/register">
+            <Register />
           </Route>
           <Route path="/">
             <Home />
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PageLayout from '../../Components/PageLayout/PageLayout';
+
+const Register = () => {
+  return <PageLayout>Registration</PageLayout>;
+};
+
+export default Register;
